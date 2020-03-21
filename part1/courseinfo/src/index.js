93efb4b500b4697fe08a7174f21a2c4ba67dac73
Stefan Nikolic <stefan.nikolic@united.cloud>
2020-03-21 11:39:43 +0100

    course information step4

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -5,12 +5,12 @@ const Header = ({ course }) => {
   return <h1>{course}</h1>;
 };
 
-const Content = ({ part: { name, exercises } }) => {
-  return (
-    <p>
-      {name} {exercises}
+const Content = ({ parts }) => {
+  return parts.map(part => (
+    <p key={part.name}>
+      {part.name} {part.exercises}
     </p>
-  );
+  ));
 };
 
 const Total = ({ exercises }) => {
@@ -25,26 +25,26 @@ const Total = ({ exercises }) => {
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7
+    },
+    {
+      name: "State of a component",
+      exercises: 14
+    }
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} />
-      <Content part={part2} />
-      <Content part={part3} />
-      <Total exercises={[part1, part2, part3]} />
+      <Content parts={parts} />
+      <Total exercises={parts} />
     </div>
   );
 };
