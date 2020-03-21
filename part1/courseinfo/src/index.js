10ed19eb670c49be3831aa989a9e12e468ca4543
Stefan Nikolic <stefan.nikolic@united.cloud>
2020-03-21 11:42:23 +0100

    course information step5

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const Header = ({ course }) => {
-  return <h1>{course}</h1>;
+  return <h1>{course.name}</h1>;
 };
 
 const Content = ({ parts }) => {
@@ -14,7 +14,6 @@ const Content = ({ parts }) => {
 };
 
 const Total = ({ exercises }) => {
-  console.log("PART", exercises);
   return (
     <p>
       Number of exercises{" "}
@@ -24,27 +23,29 @@ const Total = ({ exercises }) => {
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7
-    },
-    {
-      name: "State of a component",
-      exercises: 14
-    }
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7
+      },
+      {
+        name: "State of a component",
+        exercises: 14
+      }
+    ]
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total exercises={parts} />
+      <Content parts={course.parts} />
+      <Total exercises={course.parts} />
     </div>
   );
 };
