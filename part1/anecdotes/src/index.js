1f2f4bf093e07f08377fbb3e85cc16424eb5b754
Stefan Nikolic <stefan.nikolic@united.cloud>
2020-03-21 20:43:31 +0100

    anecdotes

---
+++ part1/anecdotes/src/index.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const App = ({ anecdotes }) => {
+  const [selected, setSelected] = useState(0);
+  const [vote, setVote] = useState([]);
+  const [max, setMax] = useState(0);
+  const [anecdote, setAnecdote] = useState(null);
+
+  const nextAnecdote = () => {
+    const random = Math.floor(Math.random() * anecdotes.length);
+    setSelected(random);
+  };
+
+  const voteForAnecdote = () => {
+    setVote([...vote, anecdotes[selected]]);
+  };
+
+  const reducer = (vote, selected) => {
+    if (!vote[selected]) {
+      vote[selected] = 1;
+    } else {
+      vote[selected] = vote[selected] + 1;
+    }
+    return vote;
+  };
+
+  const numberOfVotes = vote.reduce(reducer, {});
+
+  for (let [key, value] of Object.entries(numberOfVotes)) {
+    if (value > max) {
+      setMax(value);
+      setAnecdote(key);
+    }
+  }
+
+  return (
+    <div>
+      <p>{anecdotes[selected]}</p>
+      <button onClick={voteForAnecdote}>vote</button>
+      <button onClick={nextAnecdote}>next anecdote</button>
+      <p>{anecdote}</p>
+      {max !== 0 && <p>has {max} votes</p>}
+    </div>
+  );
+};
+
+const anecdotes = [
+  "If it hurts, do it more often",
+  "Adding manpower to a late software project makes it later!",
+  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+  "Premature optimization is the root of all evil.",
+  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it."
+];
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App anecdotes={anecdotes} />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
